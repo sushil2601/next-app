7816524acf9c5a6a9141ae3258ad27ce0352dab5
sushil2601 <sushilsuman2355@gmail.com>
2025-08-16 18:30:16 +0530

    Route group

---
+++ src/app/(auth)/forgot-password/page.js
@@ -0,0 +1,5 @@
+const ForgotPassword = () =>{
+    return <h1>Forgot password page</h1>
+}
+
+export default ForgotPassword;
---
+++ src/app/_lib/page.js
@@ -0,0 +1,5 @@
+const PrivateFolder = () =>{
+    return <h1>This is the private folder</h1>
+}
+
+export default PrivateFolder;
